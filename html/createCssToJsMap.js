9e55c0c7de198013ce8691b595524e5de0a10a22
mack-wang <641212003@qq.com>
2019-01-22 15:03:15 +0800

    添加css map生成器

--- html/createCssToJsMap.js
+++ html/createCssToJsMap.js
@@ -3,19 +3,37 @@ var path = require('path');//解析需要遍历的文件夹
 var filePath = path.resolve('../dist/component/');
 
 // node createCssToJsMap.js >> cssMap.js
-// 检查-和\符号，检查switch
+
 
 fs.readdir(filePath, function (err, files) {
     if (err) {
         console.warn(err)
     } else {
+        for (var i = 0; i < files.length; i++) {
+
+        }
         files.forEach(function (filename) {
             var fileDir = path.join(filePath, filename);
             fs.stat(fileDir, function (error, stats) {
                 if (error) {
                     console.warn('获取文件stats失败');
                 } else {
-                    console.log('export const '+filename.split('.')[0].replace('-','')+' = `' +fs.readFileSync(fileDir, 'utf-8') + '`;')
+                    var name = filename.split('.')[0].replace(/-/g, '')
+                    var content = fs.readFileSync(fileDir, 'utf-8')
+
+                    if (name === 'switch') {
+                        name = 'switchs'
+                    }
+                    if (name === 'alert') {
+                        name = 'alerts'
+                    }
+                    if (name === 'checkbox') {
+                        content = content.replace(/\\/g, '\\\\')
+                    }
+                    if (name === 'iconfont') {
+                    } else {
+                        console.log('export const ' + name + ' = `' + content + '`;')
+                    }
                 }
             })
         });
